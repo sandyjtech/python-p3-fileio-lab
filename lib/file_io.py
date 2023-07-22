9f67b0b5e12af0f546f48183aaa6b5e733c3c9c0
sandyjtech <sandyjtech@gmail.com>
2023-07-22 14:03:04 -0700

    added write, append and read functions

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,17 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name) + '.txt' 
+    with open(file_name, 'w') as file:
+        file.write(file_content)
+        print(f"Successfully wrote to {file_name}")
 
-def append_file(file_name, append_content):
-    pass
+def append_file(file_name, file_content):
+    file_name = str(file_name) + '.txt' 
+    with open(file_name, 'a') as file:
+        file.write(file_content)
+        print(f"Successfully appended to {file_name}")
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name) + '.txt' 
+    with open(file_name, 'r') as file:
+        content = file.read()
+        return content
